72ddbad062c453f471f56ed5d4ad6b51447aea1d
Arjun Wadhwa <arjun.wadhwa@walmartlabs.com>
2018-06-18 00:03:43 +0530

    Minor fix

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
+var bodyParser = require("body-parser");
 
-app.listen(3001, function() {
-    console.log("Server Running on port 3000.");
-});
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+var port = Number(process.env.PORT || 5002);
+app.listen(port);
+console.log("Server Running")
