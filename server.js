04ab737c807a302d633d91eff4dba2138e6b1b9f
Arjun Wadhwa <arjun.wadhwa@walmartlabs.com>
2018-06-18 00:00:21 +0530

    Minor fix

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000, function() {
+app.listen(3001, function() {
     console.log("Server Running on port 3000.");
 });
